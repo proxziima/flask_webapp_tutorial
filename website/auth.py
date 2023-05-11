ab4a533a19efcaea2c8241776b599928c9e2e74e
Felipe Vinicius Santos Queiroz <felipe.queiroz@scaladatacenters.com>
2023-05-11 18:02:46 -0300

    :pencil: Add Flashing Messages feature

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html.j2")
+    data = request.form
+    print(data)
+    return render_template("login.html.j2", boolean = False)
 
 @auth.route('/logout')
 def logout():
     return "<h1>Logout</h1>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error') 
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html.j2")
