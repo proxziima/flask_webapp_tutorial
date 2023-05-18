c65f531da0ae2b3b4c114891f9e90d43ed02718f
Felipe Vinicius Santos Queiroz <felipe.queiroz@scaladatacenters.com>
2023-05-18 18:15:20 -0300

    :pencil: Add login function

--- website/auth.py
+++ website/auth.py
@@ -7,8 +7,20 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first() # Verifica se o email existe no banco de dados
+
+        if user:
+            if check_password_hash(user.password, password): 
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error') # Se o email não existir, retorna um erro
+    
     return render_template("login.html.j2", boolean = False)
 
 @auth.route('/logout')
@@ -23,7 +35,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first() # Verifica se o email existe no banco de dados
+
+        if user:
+            flash('Email already exists.', category='error') # Se o email já existir, retorna um erro
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error') 
